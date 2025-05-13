22b8589cfda21d887c38a7b339eafcc51fbc9b8d
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-05-13 16:14:16 +0600

    Accepted

--- Practice/2025/May/D_Palindromic_Naming_Convention.cpp
+++ Practice/2025/May/D_Palindromic_Naming_Convention.cpp
@@ -48,43 +48,39 @@ int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + 
 int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 
-vector<int> hashPrimes = {1000000009};
-vector<vector<int>> powersOfBase,inversePowersOfBase;
-
-void findInversePower(int primes, int n){
-    const int base = 31;
-    powersOfBase.resize(primes);
-    inversePowersOfBase.resize(primes);
-    for(int i = 0; i < hashPrimes.size(); i++) {
-        powersOfBase[i].resize(n + 1);
-        inversePowersOfBase[i].resize(n + 1);
-        powersOfBase[i][0] = 1;
-        for(int j = 1; j <= n; j++){
-            powersOfBase[i][j] = (base * powersOfBase[i][j - 1]) % hashPrimes[i];
-        }
-        inversePowersOfBase[i][n] = mminvprime(powersOfBase[i][n], hashPrimes[i]);
-        for(int j = n - 1; j >= 0; j--){
-            inversePowersOfBase[i][j] = mod_mul(inversePowersOfBase[i][j + 1], base, hashPrimes[i]);
-        } 
-    }
-}
-
 struct Hashing{
     string s;
     int n;
     int primes;
+    vector<int> hashPrimes = {1000000009};
+    const int base = 31;
     vector<vector<int>> hashValues;
+    vector<vector<int>> powersOfBase;
+    vector<vector<int>> inversePowersOfBase;
     Hashing(string a){
-        primes = hashPrimes.size();
+        primes = sz(hashPrimes);
         hashValues.resize(primes);
+        powersOfBase.resize(primes);
+        inversePowersOfBase.resize(primes);
         s = a;
-        n = s.length();
-        if(inversePowersOfBase.empty()) findInversePower(primes,n);
-        for(int i = 0; i < hashPrimes.size(); i++) {
+        n = s.length(); 
+        for(int i = 0; i < sz(hashPrimes); i++) {
+            powersOfBase[i].resize(n + 1);
+            inversePowersOfBase[i].resize(n + 1);
+            powersOfBase[i][0] = 1;
+            for(int j = 1; j <= n; j++){
+                powersOfBase[i][j] = (base * powersOfBase[i][j - 1]) % hashPrimes[i];
+            }
+            inversePowersOfBase[i][n] = mminvprime(powersOfBase[i][n], hashPrimes[i]);
+            for(int j = n - 1; j >= 0; j--){
+                inversePowersOfBase[i][j] = mod_mul(inversePowersOfBase[i][j + 1], base, hashPrimes[i]);
+            } 
+        }
+        for(int i = 0; i < sz(hashPrimes); i++) {
             hashValues[i].resize(n);
             for(int j = 0; j < n; j++){
                 hashValues[i][j] = ((s[j] - 'a' + 1ll) * powersOfBase[i][j]) % hashPrimes[i];
-                hashValues[i][j] = (hashValues[i][j] + (j > 0ll ? hashValues[i][j - 1] : 0ll)) % hashPrimes[i];
+                hashValues[i][j] = (hashValues[i][j] + (j > 0 ? hashValues[i][j - 1] : 0ll)) % hashPrimes[i];
             }
         }
     }
@@ -92,23 +88,10 @@ struct Hashing{
         vector<int> hash(primes);
         for(int i = 0; i < primes; i++){
             int val1 = hashValues[i][r];
-            int val2 = l > 0ll ? hashValues[i][l - 1] : 0ll;
+            int val2 = l > 0 ? hashValues[i][l - 1] : 0ll;
             hash[i] = mod_mul(mod_sub(val1, val2, hashPrimes[i]), inversePowersOfBase[i][l], hashPrimes[i]);
         }
         return hash;
-    }
-    bool compareSubstrings(int l1, int r1, int l2, int r2){
-        if(l1 > l2){
-            swap(l1, l2);
-            swap(r1, r2);
-        }
-        for(int i = 0; i < primes; i++){
-            int val1 = mod_sub(hashValues[i][r1], (l1 > 0 ? hashValues[i][l1 - 1] : 0LL), hashPrimes[i]);
-            int val2 = mod_sub(hashValues[i][r2], (l2 > 0 ? hashValues[i][l2 - 1] : 0LL), hashPrimes[i]);
-            if(mod_mul(val1, powersOfBase[i][l2 - l1], hashPrimes[i]) != val2)
-                return false;
-        }   
-        return true;
     }
 };
 
