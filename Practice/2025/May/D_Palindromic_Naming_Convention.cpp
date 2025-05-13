f0d12ecc9301932fce279c36ab699ca9be7b0305
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-05-13 16:08:41 +0600

    Not Accepted

--- Practice/2025/May/D_Palindromic_Naming_Convention.cpp
+++ Practice/2025/May/D_Palindromic_Naming_Convention.cpp
@@ -41,23 +41,104 @@ template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_or
 
 /****************************************************************/
 
+int expo(int a, int b, int m) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res;}
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+
+vector<int> hashPrimes = {1000000009};
+vector<vector<int>> powersOfBase,inversePowersOfBase;
+
+void findInversePower(int primes, int n){
+    const int base = 31;
+    powersOfBase.resize(primes);
+    inversePowersOfBase.resize(primes);
+    for(int i = 0; i < hashPrimes.size(); i++) {
+        powersOfBase[i].resize(n + 1);
+        inversePowersOfBase[i].resize(n + 1);
+        powersOfBase[i][0] = 1;
+        for(int j = 1; j <= n; j++){
+            powersOfBase[i][j] = (base * powersOfBase[i][j - 1]) % hashPrimes[i];
+        }
+        inversePowersOfBase[i][n] = mminvprime(powersOfBase[i][n], hashPrimes[i]);
+        for(int j = n - 1; j >= 0; j--){
+            inversePowersOfBase[i][j] = mod_mul(inversePowersOfBase[i][j + 1], base, hashPrimes[i]);
+        } 
+    }
+}
+
+struct Hashing{
+    string s;
+    int n;
+    int primes;
+    vector<vector<int>> hashValues;
+    Hashing(string a){
+        primes = hashPrimes.size();
+        hashValues.resize(primes);
+        s = a;
+        n = s.length();
+        if(inversePowersOfBase.empty()) findInversePower(primes,n);
+        for(int i = 0; i < hashPrimes.size(); i++) {
+            hashValues[i].resize(n);
+            for(int j = 0; j < n; j++){
+                hashValues[i][j] = ((s[j] - 'a' + 1ll) * powersOfBase[i][j]) % hashPrimes[i];
+                hashValues[i][j] = (hashValues[i][j] + (j > 0ll ? hashValues[i][j - 1] : 0ll)) % hashPrimes[i];
+            }
+        }
+    }
+    vector<int> substringHash(int l, int r){
+        vector<int> hash(primes);
+        for(int i = 0; i < primes; i++){
+            int val1 = hashValues[i][r];
+            int val2 = l > 0ll ? hashValues[i][l - 1] : 0ll;
+            hash[i] = mod_mul(mod_sub(val1, val2, hashPrimes[i]), inversePowersOfBase[i][l], hashPrimes[i]);
+        }
+        return hash;
+    }
+    bool compareSubstrings(int l1, int r1, int l2, int r2){
+        if(l1 > l2){
+            swap(l1, l2);
+            swap(r1, r2);
+        }
+        for(int i = 0; i < primes; i++){
+            int val1 = mod_sub(hashValues[i][r1], (l1 > 0 ? hashValues[i][l1 - 1] : 0LL), hashPrimes[i]);
+            int val2 = mod_sub(hashValues[i][r2], (l2 > 0 ? hashValues[i][l2 - 1] : 0LL), hashPrimes[i]);
+            if(mod_mul(val1, powersOfBase[i][l2 - l1], hashPrimes[i]) != val2)
+                return false;
+        }   
+        return true;
+    }
+};
+
+bool palindromeFound(Hashing& original, Hashing& reversed, int l1, int r1, int n){
+    int l2 = n - r1 - 1, r2 = n - l1 - 1;
+    if(original.substringHash(l1, r1) == reversed.substringHash(l2, r2)){
+        return true;
+    }
+    return false;
+}
 
 void solve()
 {
     string str;
     cin>>str;
-    string curr = "";
-    for(int i = 0; i<sz(str); i++){
-        reverse(all(curr));
-        curr.push_back(str[i]);
-        reverse(all(curr));
-        string newStr = str+curr,rstr = newStr;
-        reverse(all(rstr));
-        if(newStr==rstr){
-            cout<<str<<" "<<curr<<endl;
-            return;
+    string ans = str;
+    reverse(all(ans));
+    int n = sz(str);
+    Hashing original(str);
+    reverse(all(str));
+    Hashing reversed(str);
+    reverse(all(str));
+    for(int i = 1; i<n; i++){
+        if(palindromeFound(original,reversed,i,n-1,n)){
+            ans = str.substr(0,i);
+            reverse(all(ans));
+            break;
         }
     }
+    cout<<str<<" "<<ans<<endl;
 }
 
 int32_t main()
