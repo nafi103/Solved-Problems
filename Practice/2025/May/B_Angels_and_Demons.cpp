1749eb033cf583a462b05ac68c770806abac62cd
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-05-16 13:52:52 +0600

    Accepted

---
+++ Practice/2025/May/B_Angels_and_Demons.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>>v;
+    v.reserve(2*m);
+    for(int i = 0; i<m; i++){
+        int t,b,e,d;
+        cin>>t>>b>>e>>d;
+        if(t==1){
+            v.push_back({b,d});
+            v.push_back({e+1,-d});
+        }else{
+            v.push_back({b,-d});
+            v.push_back({e+1,d});
+        }
+    }
+    sort(all(v));
+    int curr = 0, bc = v[0].ss, add = 0;
+    for(int i = 1; i<2*m;  i++){
+        curr+=(v[i].ff-v[i-1].ff)*bc;
+        bc+=v[i].ss;
+        add = max(add,-curr);
+    }
+    cout<<1+add<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        // google(z);
+        solve();
+    }
+}
