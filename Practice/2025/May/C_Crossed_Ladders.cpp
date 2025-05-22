4ed0e03cce07327a779bcb3e2f793ec60b7ad927
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-05-22 08:19:25 +0600

    Accepted

---
+++ Practice/2025/May/C_Crossed_Ladders.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+const double eps = 1e-12;
+double x,y,c;
+
+bool check(double len){
+    double y1 = sqrt(x*x-len*len), y2 = sqrt(y*y-len*len);
+    return (y1*y2)/(y1+y2) - c > eps;
+}
+
+double bs(double l, double r){
+    if(r-l<eps)
+        return r;
+    double mid = (l+r)/2.0;
+    if(check(mid))
+        return bs(mid+eps,r);
+    return bs(l,mid-eps);
+}
+
+void solve()
+{
+    cin>>x>>y>>c;
+    if(x>y)
+        swap(x,y);
+    cout<<bs(eps,x-eps)<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
