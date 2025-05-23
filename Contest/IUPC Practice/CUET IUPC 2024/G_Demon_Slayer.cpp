767f498220e2b1da90b62deee83a705218810daa
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-05-23 16:58:31 +0600

    Accepted

--- Contest/IUPC Practice/CUET IUPC 2024/G_Demon_Slayer.cpp
+++ Contest/IUPC Practice/CUET IUPC 2024/G_Demon_Slayer.cpp
@@ -1,80 +1,70 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define int long long
 using namespace std;
-
+#define all(x) x.begin(), x.end()
 const int MAX = 5e6 + 6;
-const pair<int, int> VERTICAL = {1, 0};
-const pair<int, int> HORIZONTAL = {0, 1};
+const int inf = 1e10;
 
-pair<int, int> calc_slope(int dy, int dx) {
-    if (dy == 0) return HORIZONTAL;
-    if (dx == 0) return VERTICAL;
-    int g = gcd(abs(dy), abs(dx));
-    dy /= g;
-    dx /= g;
-    if (dx < 0) {
-        dy *= -1;
-        dx *= -1;
+pair<int,int> calc_slope(int y, int x){
+    if(y==0){
+        return {0,0};
     }
-    return {dy, dx};
+    if(x==0){
+        return {inf,inf};
+    }
+    int g = gcd(y,x);
+    if (g != 0) {
+        y/=g;
+        x/=g;
+    }
+    if(y<0){
+        y*=-1;
+        x*=-1;
+    }
+    return {y,x};
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, m;
-    cin >> n >> m;
-
-    vector<pair<int, int>> demon(m);
-    for (auto &[x, y] : demon) {
-        cin >> x >> y;
+    cin.tie(NULL), cout.tie(NULL);
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>>demon(m);
+    for(auto &[f,s]: demon){
+        cin>>f>>s;
     }
-
-    vector<int> ans(n + 1, 1);
-
-    for (int i = 0; i < m; i++) {
-        int same_point = 0;
-        map<pair<int, int>, int> slope_count;
-
-        for (int j = 0; j < m; j++) {
-            if (i == j) continue;
-            if (demon[i] == demon[j]) {
-                same_point++;
+    vector<int>ans(n+1,1);
+    for(int i = 0; i<m;  i++){
+        int add = 0;
+        map<pair<int,int>, int>slope;
+        for(int j = 0; j<m; j++){
+            if(i==j)
+                continue;
+            if(demon[i]==demon[j]){
+                add++;
                 continue;
             }
-
-            auto slope = calc_slope(demon[j].second - demon[i].second,
-                                    demon[j].first - demon[i].first);
-            slope_count[slope]++;
+            slope[calc_slope(demon[j].second-demon[i].second,demon[j].first-demon[i].first)]++;
         }
-
-        for (auto &[slope, cnt] : slope_count) {
-            if (slope == HORIZONTAL || slope == VERTICAL) {
-                if (slope == VERTICAL && demon[i].first >= 1 && demon[i].first <= n)
-                    ans[demon[i].first] = max(ans[demon[i].first], cnt + 1);
+        for(auto &[slp,cnt]: slope){
+            if(slp==make_pair(0ll,0ll) or !slp.first){
                 continue;
             }
-
-            int dy = slope.first;
-            int dx = slope.second;
-            int y = demon[i].second;
-            int x = demon[i].first;
-
-            if (dy != 0 && y % dy == 0) {
-                int steps = y / dy;
-                int laser_x = x - steps * dx;
-                if (laser_x >= 1 && laser_x <= n) {
-                    ans[laser_x] = max(ans[laser_x], cnt + same_point + 1);
+            if(slp==make_pair(inf,inf) and demon[i].first>=1 and demon[i].first<=n){
+                ans[demon[i].first] = max(ans[demon[i].first],cnt+1);
+            }else{
+                int my_y = demon[i].second, slope_y = slp.first;
+                if(slope_y != 0 and my_y % slope_y==0){
+                    int laser_x = demon[i].first - (my_y/slope_y)*slp.second;
+                    if(laser_x>=1 and laser_x<=n){
+                        ans[laser_x] = max(ans[laser_x],cnt+add+1);
+                    }
                 }
             }
         }
     }
-
-    for (int i = 1; i <= n; i++) {
-        cout << ans[i] << " ";
+    for(int i = 1; i<=n; i++){
+        cout<<ans[i]<<" ";
     }
-    cout << "\n";
-
     return 0;
 }
