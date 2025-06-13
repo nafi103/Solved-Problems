8ff5400dd2b4c721d98503b96da375a692de70c4
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-13 12:22:27 +0600

    Accepted

---
+++ Practice/2025/June/D_Air_Ports.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+vector<int>parent, _size; 
+
+int find(int i){ 
+    if(parent[i]==i) return i; 
+    return parent[i] = find(parent[i]); 
+} 
+
+int size(int a){ 
+    a = find(a); 
+    return _size[a]; 
+} 
+
+void Union(int a, int b){ 
+    a = find(a); 
+    b = find(b); 
+    if(a==b) return; 
+    if(_size[a]<_size[b]) swap(a,b);
+    parent[b] = a; 
+    _size[a]+=_size[b]; 
+}
+
+void solve()
+{
+    parent.clear();
+    _size.clear();
+    int n,m,a_cost;
+    cin>>n>>m>>a_cost; 
+    parent.resize(n+1); 
+    _size.assign(n+1,1); 
+    for(int i = 1; i<=n; i++) 
+        parent[i] = i; 
+    vector<array<int,3>>edges;
+    while(m--){ 
+        int u,v,w; 
+        cin>>u>>v>>w;
+        edges.push_back({w,u,v});
+    }
+    sort(all(edges));
+    int airports = n,cost = 0;
+    for(auto &[w,u,v]: edges){
+        if(find(u)!=find(v) and w<a_cost){
+            airports--;
+            cost+=w;
+            Union(u,v);
+        }
+    }
+    cout<<airports*a_cost + cost<<" "<<airports<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
