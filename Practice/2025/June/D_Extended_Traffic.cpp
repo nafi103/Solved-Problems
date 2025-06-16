32c69fb88f009af91961efced8eefa00c9626d28
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-16 23:39:24 +0600

    Accepted

---
+++ Practice/2025/June/D_Extended_Traffic.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e17+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+int cube(int x){
+    return x*x*x;
+}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>traffic(n);
+    readv(traffic);
+    int m;
+    cin>>m;
+    vector<array<int,3>>edges;
+    while(m--){
+        int u,v;
+        cin>>u>>v;
+        u--;v--;
+        edges.push_back({cube(traffic[v]-traffic[u]),u,v});
+    }
+    vector<int>dist(n,inf);
+    dist[0] = 0;
+    for(int i = 0; i<n-1; i++){
+        for(auto [d,u,v]: edges){
+            if(dist[u]!=inf) dist[v] = min(dist[v],dist[u]+d);
+        }
+    }
+    int q,x;
+    cin>>q;
+    while(q--){
+        cin>>x;
+        x--;
+        if(dist[x]<3 or dist[x]==inf){
+            cout<<'?'<<endl;
+        }else{
+            cout<<dist[x]<<endl;
+        }
+    }
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<":\n";
+        solve();
+    }
+}
