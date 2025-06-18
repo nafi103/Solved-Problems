eabb184ded0edca17ee36ae6bcb2db709e7290f2
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-18 21:48:35 +0600

    Accepted

---
+++ Practice/2025/June/B_Just_another_Robbery.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+
+void solve()
+{
+    double mx;
+    int m, sum = 0;
+    cin>>mx>>m;
+    vector<pair<int,double>>p(m);
+    for(auto &[f,s]: p){
+        cin>>f>>s;
+        sum+=f;
+    }
+    vector<double>dp(sum+1,0);
+    dp[0] = 1.0;
+    for(int i = 0; i<m; i++){
+        for(int j = sum; j-p[i].ff>=0; j--){
+            dp[j] = max(dp[j], dp[j-p[i].ff]*(1.0-p[i].ss));
+        }
+    }
+    for(int i = sum; i>=1; i--){
+        if(1.0-dp[i]<mx){
+            cout<<i<<endl;
+            return;
+        }
+    }
+    cout<<0<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
