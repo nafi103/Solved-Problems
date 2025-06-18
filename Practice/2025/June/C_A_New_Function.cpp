f7080e172c767531583a8a7bcce36f0a8d8fcaf1
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-18 10:55:09 +0600

    Accepted

---
+++ Practice/2025/June/C_A_New_Function.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int unsigned long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+int n, curr;
+
+int f(int n){
+    return (n*(n+1))/2;
+}
+
+void solve()
+{
+    int ans = 0;
+    cin>>n;
+    curr = 1;
+    int fn = f(n);
+    bool f1 = true, f2 = true;
+    while(curr<=n){
+        int val = n/curr;
+        int _next = n/val;
+        int range_sum = f(_next) - f(curr-1);
+        ans+=(range_sum*val);
+        if(f1 and ans>=n-1){
+            ans-=n-1;
+            f1 = false;
+        }
+        if(f2 and ans>=fn){
+            ans-=fn;
+            f2 = false;
+        }
+        curr = _next+1;
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
