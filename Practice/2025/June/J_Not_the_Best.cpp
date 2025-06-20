b085f16a00b6d13cf791e8be41bb173f67e3b96a
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-20 13:32:09 +0600

    Accepted

---
+++ Practice/2025/June/J_Not_the_Best.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+struct Graph{
+    vector<vector<pair<int,int>>>v;
+    int n;
+    Graph(int N){
+        v.resize(N+1);
+        n = N;
+    }
+    void add(int uu, int vv, int wt){
+        v[uu].pb({vv,wt});
+        v[vv].pb({uu,wt});
+    }
+    vector<int> dijkstra(int src)
+    {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> distance(n+1, inf), second_distance(n+1, inf);
+        distance[src] = 0;
+        pq.push({0, src});
+        while (!pq.empty())
+        {
+            auto [d,node] = pq.top();
+            pq.pop();
+            if(d>distance[node] and d>second_distance[node]){
+                continue;
+            }
+            for (auto &[nbr,wt] : v[node])
+            {
+                if (distance[nbr] > d + wt)
+                {
+                    second_distance[nbr] = distance[nbr];
+                    distance[nbr] = d + wt;
+                    pq.push({distance[nbr], nbr});
+                }else if(d + wt != distance[nbr] and second_distance[nbr] > d + wt){
+                    second_distance[nbr] = d + wt;
+                    pq.push({second_distance[nbr] , nbr});
+                }
+            }
+        }
+        return second_distance;
+    }
+};
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    Graph g(n);
+    while(m--){
+        int u,v,w;
+        cin>>u>>v>>w;
+        g.add(u,v,w);
+    }
+    vector<int> second_distance = g.dijkstra(1);
+    cout<<second_distance[n]<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
