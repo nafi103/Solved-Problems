a3b33137e3c372bcb484726ef6d41bf6813e3f74
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-06-26 17:51:56 +0600

    Accepted

---
+++ Practice/2025/June/A_Fi_Binary_Number.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+vector<int> fibo = {1,1};
+
+void solve()
+{
+    string ans = "";
+    int n;
+    cin>>n;
+    for(int i = 0; i<sz(fibo); i++){
+        if(fibo[i]<=n){
+            n-=fibo[i];
+            ans.push_back('1');
+        }else{
+            if(sz(ans))
+                ans.push_back('0');
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    for(int i = 1; fibo[i]+fibo[i-1]<1e9; i++){
+        fibo.push_back(fibo[i]+fibo[i-1]);
+    }
+    reverse(all(fibo));
+    fibo.pop_back();
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
