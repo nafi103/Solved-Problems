7c4d2f51bf6d470127fc0a8ce0b00e9afe4a7549
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-07-17 16:49:52 +0600

    Accepted, implementation for divisible group sums problem using dynamic programming

---
+++ Practice/2025/July/B_Divisible_Group_Sums.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+int n,q;
+vector<int>v;
+int m,r;
+vector<vector<vector<int>>>dp; // ith index mod value j, m left
+
+int f(int pos, int sum, int rem){
+    if(pos==n)
+        return rem==0 and sum==0;
+    if(rem==0)
+        return sum==0;
+    int &ans = dp[pos][sum][rem];
+    if(ans!=-1)
+        return ans;
+    ans = 0;
+    return ans = f(pos+1, (sum+(v[pos]%m)+m)%m, rem-1) + f(pos+1, sum, rem);
+}
+
+void solve()
+{
+    cin>>n>>q;
+    v.resize(n);
+    readv(v);
+    while(q--){
+        cin>>m>>r;
+        dp.clear();
+        dp.assign(n,vector<vector<int>>(m,vector<int>(r+1,-1)));
+        cout<<f(0,0,r)<<endl;
+    }
+    v.clear();
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<":\n";
+        solve();
+    }
+}
