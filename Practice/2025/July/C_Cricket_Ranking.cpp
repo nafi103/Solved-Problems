c85f08c656e0b12f574b6e991c3ae181cd4ce149
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-07-21 17:35:46 +0600

    Accepted, implementation of the cricket ranking problem using stars and bars + IEP

---
+++ Practice/2025/July/C_Cricket_Ranking.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 100000007;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+int expo(int a, int b, int m) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res;}
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+
+const int N = 1e6 + 20;
+vector<int>fact(N), ifact(N);
+
+void solve()
+{
+    int n,target;
+    cin>>n>>target;
+    vector<int>v(n);
+    for(int i = 0; i<n; i++){
+        int l,r;
+        cin>>l>>r;
+        v[i] = r-l;
+        target -= l;
+    }
+    if(target<0){
+        cout<<0<<endl;
+        return;
+    }
+    int ans = ((fact[target+n-1]*ifact[target])%mod * ifact[n-1])%mod, r = (1<<n), bad = 0;
+    for(int mask = 1; mask<r; mask++){
+        int set_bit = __builtin_popcount(mask),new_target = target;
+        for(int j = 0; j<n; j++){
+            if(mask&(1<<j)){
+                new_target-=(v[j]+1);
+            }
+        }
+        if(new_target<0)
+            continue;
+        int operand = ((fact[new_target+n-1]*ifact[new_target])%mod * ifact[n-1])%mod;
+        if(set_bit&1){
+            bad = (bad+operand)%mod;
+        }else{
+            bad = ((bad-operand)%mod + mod)%mod;
+        }
+    }
+    cout<<((ans-bad)%mod + mod)%mod<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    fact[0] = ifact[0] = 1;
+    for(int i = 1; i<N; i++){
+        fact[i] = (fact[i-1]*i)%mod;
+    }
+    ifact[N-1] = mminvprime(fact[N-1],mod);
+    for(int i = N-2; i>0; i--){
+        ifact[i] = (ifact[i+1]*(i+1))%mod;
+    }
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
