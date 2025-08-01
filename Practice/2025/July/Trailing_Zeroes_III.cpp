9c1e5025dd3671ab49ae2a7c2f0cd6e066ccb25a
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-08-01 21:26:12 +0600

    Accepted, Implement solution for calculating trailing zeroes in factorials using binary search

---
+++ Practice/2025/July/Trailing_Zeroes_III.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define writev(v)     \
+    for (auto &x : v) \
+    cout << x << " "; \
+    cout<<endl
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int ans = inf;
+    int l = 5, r = 1e10;
+    while(l<=r){
+        int mid = (l+r)/2;
+        int fives = mid/5;
+        for(int i = 5*5; i<=mid; i*=5){
+            fives+=mid/i;
+        }
+        if(fives>=n){
+            if(fives==n)
+                ans=min(ans,mid);
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+    }
+    if(ans==inf)
+        cout<<"impossible"<<endl;
+    else
+        cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
