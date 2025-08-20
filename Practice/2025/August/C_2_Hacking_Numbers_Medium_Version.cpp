92a4a1850c89bdc23d28c112e53b92b745bee7c9
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-08-20 19:02:08 +0600

    Implemented solution using the logic 9 | S(x) if 9 | x

---
+++ Practice/2025/August/C_2_Hacking_Numbers_Medium_Version.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+
+void solve()
+{
+    int n,x;
+    cin>>n;
+    cout<<"mul 18"<<endl;
+    cin>>x;
+    cout<<"digit"<<endl;
+    cin>>x;
+    cout<<"digit"<<endl;
+    cin>>x;
+    cout<<"add "<<n-9<<endl;
+    cin>>x;
+    cout<<"!"<<endl;
+    cin>>x;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for(int z = 1; z<=t; z++){
+        // cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
