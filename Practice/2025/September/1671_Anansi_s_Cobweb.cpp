053613a14e1a0ce029175b5f21b24aca52be1983
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-09-21 09:04:42 +0600

    Accepted Easy Problem

---
+++ Practice/2025/September/1671_Anansi_s_Cobweb.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+/****************************************************************/
+
+#define int long long
+#define pi acos(-1.0)
+const int mod = 998244353;
+#define inf 1e18+10
+#define sz(x) (int)(x).size()
+#define LSOne(x) ((x)&(-x))
+#define all(x) x.begin(), x.end()
+#define readv(v)      \
+    for (auto &x : v) \
+    cin >> x
+#define endl "\n"
+#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
+
+/****************************************************************/
+struct DSU{
+    int components;
+    vector<int>parent, _size;
+
+    DSU(int n){
+        components = n;
+        parent.resize(n);
+        iota(all(parent),0);
+        _size.assign(n,1); 
+    }
+ 
+    int find(int i){ 
+        if(parent[i]==i) return i; 
+        return parent[i] = find(parent[i]); 
+    } 
+    
+    int size(int a){ 
+        a = find(a); 
+        return _size[a]; 
+    }
+    
+    void Union(int a, int b){ 
+        a = find(a); 
+        b = find(b); 
+        if(a==b) return; 
+        components--;
+        if(_size[a]<_size[b]) swap(a,b);
+        parent[b] = a; 
+        _size[a]+=_size[b]; 
+    }
+};
+
+void solve()
+{
+    int n,m,q;
+    cin>>n>>m;
+    DSU uf(n);
+    vector<bool>take(m,true);
+    vector<pair<int,int>>edges(m);
+    for(auto &[u,v]: edges){
+        cin>>u>>v;
+        u--;v--;
+    }
+    cin>>q;
+    vector<int>query(q),ans;
+    ans.reserve(m);
+    for(auto &x: query){
+        cin>>x;
+        x--;
+        take[x] = false;
+    }
+    for(int i = 0; i<m; i++){
+        if(take[i]){
+            uf.Union(edges[i].first,edges[i].second);
+        }
+    }
+    while(!query.empty()){
+        ans.push_back(uf.components);
+        int i = query.back();
+        query.pop_back();
+        uf.Union(edges[i].first,edges[i].second);
+    }
+    reverse(all(ans));
+    for(auto &x: ans)
+        cout<<x<<" ";
+    cout<<endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    fastIO;
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    for(int z = 1; z<=t; z++){
+        // cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
