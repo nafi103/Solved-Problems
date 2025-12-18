463beee5b1fe92715a834db3118eadfb4b9b3421
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-12-18 11:25:10 +0600

    Accepted

--- Practice/2025/December/Sakura_Adachi_and_Optimal_Sequences.cpp
+++ Practice/2025/December/Sakura_Adachi_and_Optimal_Sequences.cpp
@@ -19,99 +19,71 @@ const int inf = 1e18 + 10;
 #endif
 
 /****************************************************************/
-const int N = mod;
+const int N = mod + 10;
 int n,fact[N];
-
-bool check(int add, int &c, int &t, int &mult){
-	return ((c + add) << mult) <= t;
-}
-
-int bs(int l, int r, int &c, int &t, int &mult){
-	if(l > r)
-		return r;
-	int mid = (l + r) >> 1;
-	if(check(mid, c, t, mult))
-		return bs(mid + 1, r, c , t, mult);
-	return bs(l, mid - 1, c, t, mult);
-}
 
 int expo(int a, int b, int m) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res;}
 
 int mminvprime(int a) {return expo(a, mod - 2, mod);}
 
-int calc1(vector<int> &a, vector<int> &b, int mult, int &operation){
-	debug(mult)
-	vector<int> v;
-	for(int i = 0; i < n; i++){
-		int add = bs(0,mod,a[i],b[i],mult);
-		debug(a[i]) debug(b[i]) debug(add)
-		if(add){
-			v.push_back(add);
-			operation += add;
-			a[i] += add;
-		}
-	}
-	if(operation >= mod)
-		return 0;
-	int ans = fact[operation];
-	for(auto &x: v){
-		ans = (ans * mminvprime(fact[x])) % mod;
-	}
-	return ans % mod;
-}
-
 int go(int a, int b){
-	int res = 0;
-	while((a<<1) <= b){
-		a<<=1;
-		res++;
-	}
-	return res;
+    int res = 0;
+    while((a<<1) <= b){
+        a<<=1;
+        res++;
+    }
+    return res;
 }
 
 void solve()
 {
-    int left = 1, right = 1, operation = 0, right_add = 0;
+    int operation = 0, way = 1;
     cin >> n;
     vector<int> a(n), b(n);
     for(auto &x: a)
-    	cin >> x;
+        cin >> x;
     for(auto &x: b)
-    	cin >> x;
+        cin >> x;
     int mult = inf;
     for(int i = 0; i < n; i++){
-    	if(a[i] == b[i]){
-    		mult = 0;
-    		break;
-    	}
-    	mult = min(mult,go(a[i], b[i]));
+        if(a[i] == b[i]){
+            mult = 0;
+            break;
+        }
+        mult = min(mult,go(a[i], b[i]));
     }
-    if(mult != 0){
-    	left = calc1(a,b,mult,operation);
-    	operation += mult;
-    	for(auto &x: a){
-    		x<<=mult;
-    	}
-    }
-    vector<int> v;
+    vector<vector<int>> steps;
     for(int i = 0; i < n; i++){
-    	int add = b[i] - a[i];
-    	right_add += add;
-    	operation += add;
-    	if(add)
-    		v.push_back(add);
+        vector<int> tmp;
+        int y = b[i], x = a[i], rem = mult;
+        while(rem--){
+            if(y&1)
+                tmp.push_back(1);
+            else 
+                tmp.push_back(0);
+            y >>= 1;
+        }
+        tmp.push_back(y-x);
+        operation += accumulate(all(tmp),0ll);
+        steps.push_back(tmp);
     }
-    debug(a) debug(b) debug(v)
-   	debug(right_add)
-    if(right_add >= mod){
-    	right = 0;
-    }else{
-    	right = fact[right_add];
-    	for(auto &x: v){
-			right = (right * mminvprime(fact[x])) % mod;
-		}
+    operation += mult;
+    for(int j = 0; j <= mult; j++){
+        int this_step = 0;
+        for(int i = 0; i < n; i++){
+            this_step += steps[i][j];
+        }
+        if(this_step >= mod){
+            way = 0;
+            break;
+        }
+        int p = fact[this_step];
+        for(int i = 0; i < n; i++){
+            p = (p * mminvprime(fact[steps[i][j]])) % mod;
+        }
+        way = (way * p) % mod;
     }
-    cout << operation << " " << (left * right) % mod << endl;
+    cout << operation <<" " << way << endl;
 }
 
 int32_t main()
@@ -123,7 +95,7 @@ int32_t main()
     cout.setf(ios::fixed);
     fact[0] = 1;
     for(int i = 1; i < N; i++){
-    	fact[i] = (fact[i - 1] * i) % mod;
+        fact[i] = (fact[i - 1] * i) % mod;
     }
     int t = 1;
     cin >> t;
