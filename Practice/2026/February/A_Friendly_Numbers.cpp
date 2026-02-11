6a226eb617958258c1b05027e03c595204a66b85
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2026-02-11 23:58:38 +0600

    Accepted

---
+++ Practice/2026/February/A_Friendly_Numbers.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/****************************************************************/
+
+#define int long long
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+const int mod = 998244353;
+const int inf = 1e18 + 10;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << "Line " << __LINE__ << ": " << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+int d(int x){
+    int s = 0;
+    while(x > 0){
+        s += x % 10;
+        x /= 10;
+    }
+    return s;
+}
+
+void solve()
+{
+    int x;
+    cin >> x;
+    int ans = 0;
+    for (int y = x + 1000; y >= x; y--){
+        if(y - d(y) == x)
+            ans++;
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for (int z = 1; z <= t; z++)
+    {
+        // cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
