43cc297f087384588fb6b81a3d88f0fe7cc5e9a2
Nasif Shahrier <nasifshahrierrifat@gmail.com>
2026-06-12 13:17:03 +0600

    Accepted

---
+++ Practice/2026/June/Permutation_Transmission.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/****************************************************************/
+
+#define int long long
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+const int mod = 998244353;
+const int inf = 1e18 + 10;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << "Line " << __LINE__ << ": " << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+int fact[21];
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int m = log2(n) + 1;
+    vector<pair<int, string>> bit(m);
+    for(auto &[cnt, str]: bit){
+        cin >> str;
+        cnt = count(all(str), '1');
+    }
+
+    sort(all(bit), [&](pair<int, string> &a, pair<int, string> &b){
+        return a.first > b.first;
+    });
+
+    vector<int> p(n, 0);
+    for(int i = 0; i < m; i++){
+        auto &[val, str] = bit[i];
+        for(int j = 0; j < n; j++){
+            if(str[j] == '1')
+                p[j] += (1 << i);
+        }
+    }
+
+    sort(all(p));
+    for(int i = 0; i < n; i++){
+        if(p[i] != i + 1){
+            cout << 0 << endl;
+            return;
+        }
+    }
+
+    int cnt = 1, ans = 1;
+    for(int i = 1; i < m; i++){
+        if(bit[i].first != bit[i - 1].first){
+            ans *= fact[cnt];
+            cnt = 1;
+        }else{
+            cnt++;
+        }
+    }
+
+    ans *= fact[cnt];
+
+    cout << ans << endl;
+}  
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+
+    fact[0] = 1;
+    for(int i = 1; i < 21; i++){
+        fact[i] = fact[i - 1] * i;
+    }
+
+    cin >> t;
+    for (int z = 1; z <= t; z++)
+    {
+        // cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
---
+++ Practice/2026/June/We_Be_Flipping_HV.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/****************************************************************/
+
+#define int long long
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+const int mod = 998244353;
+const int inf = 1e18 + 10;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#define debug(x) cerr << "Line " << __LINE__ << ": " << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(...)
+#endif
+
+/****************************************************************/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> ans, arr(n);
+
+    for(auto &x: arr)
+        cin >> x;
+
+    int res = accumulate(all(arr), 0ll), id = -1;
+    for(int i = 0, tot = res, sum = 0; i < n; i++){
+        tot -= arr[i];
+        if(arr[i] > 0){
+            if(tot + sum - arr[i] > res){
+                res = tot + sum - arr[i];
+                id = i;
+            }
+        }
+        sum += abs(arr[i]);
+    }
+
+    if(id == -1){
+        cout << "0\n\n";
+        return;
+    }
+
+    for(int i = id - 1, p = 0; i >= 0; i--){
+        int flip = (arr[i] > 0);
+
+        if((flip and !p) or (!flip and p)){
+            p ^= 1;
+            ans.push_back(i + 1);
+        }
+    }
+    ans.push_back(id + 1);
+
+    cout << sz(ans) << endl;
+    for(auto &x: ans)
+        cout << x << " ";
+    cout << endl;
+}
+
+int32_t main()
+{
+    // freopen("paint.in", "r", stdin);
+    // freopen("paint.out", "w", stdout);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cout.precision(10);
+    cout.setf(ios::fixed);
+    int t = 1;
+    cin >> t;
+    for (int z = 1; z <= t; z++)
+    {
+        // cout<<"Case "<<z<<": ";
+        solve();
+    }
+}
